2a2c4f8410a331966fb50cae240c53733a6dd7b7
andrewLivancov <gilamrot@gmail.com>
2020-11-08 20:17:35 +0300

    book 200

---
+++ book-200.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php ## Слияние списков при помощи оператора +
+$good = ["Julian Arahanga", "Matt Doran", "Belinda McClory"];
+$bad = ["Paul Goddard", "Robert Taylor"];
+$ugly = ["Clint Eastwood"];
+$all = $good + $bad + $ugly;
+print_r($all);
+?>
+</body>
+</html>
