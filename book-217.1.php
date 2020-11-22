a59e60232efa387ecaa61c402f32e399f1cf0101
andrewLivancov <gilamrot@gmail.com>
2020-11-22 23:17:30 +0300

    book 217.1

---
+++ book-217.1.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php ## Использование ...
+function toomanyargs($fst, $snd, $thd, $fth)
+{
+	echo "Первый параметр: $fst<br />";
+	echo "Второй параметр: $snd<br />";
+	echo "Третий параметр: $thd<br />";
+	echo "Четвертый параметр: $fth<br />";
+}
+// Отображаем строки одну под другой
+$planets = ["Меркурий", "Венера", "Земля", "Марс"];
+toomanyargs(...$planets);
+  ?>
+</body>
+</html>
