f69093dc0f0b1b02e0b238b4101b77be753e44b9
andrewLivancov <gilamrot@gmail.com>
2020-11-22 20:59:37 +0300

    book 212.1

---
+++ book-212.1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+function mySqr($n)
+{
+return $n * $n;  
+}
+$value = mySqr(4);
+echo $value, "<br>"; // выводит 16
+echo mySqr(10); // выводит 100
+?>
+</body>
+</html>
