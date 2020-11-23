d8033a89629d89bcebdef327fbb11579ba28a475
andrewLivancov <gilamrot@gmail.com>
2020-11-23 12:29:15 +0300

    book 218

---
+++ book-218.1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php ## Строгая типизация 
+declare(strict_types = 1);
+function sum(int $fst, int $snd) : int
+{
+	return $fst + $snd;
+}
+echo sum(2, 2); // 4
+echo sum(2.5, 2.5); 
+  ?>
+</body>
+</html>
