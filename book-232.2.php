453b4a811f4610b22e012028e3f1d137817ed439
andrewLivancov <gilamrot@gmail.com>
2021-02-08 23:23:44 +0300

    book 232.2

---
+++ book-232.2.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$a = 100;
+function &r()
+{
+	global $a;
+	return $a;
+}
+$b =& r();
+$b = 0;
+echo $a;
+  ?>
+</body>
+</html>
