73e892e6f066ec07dc8d1937b31ddf1a26b4306d
andrewLivancov <gilamrot@gmail.com>
2021-02-11 21:46:16 +0300

    book 243.2

---
+++ book-243.2.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+function crange($size)
+{
+	for($i = 0; $i < $size; $i++) {
+		yield $i;
+	}
+}
+
+$range = crange(1024000);
+foreach($range as $i) echo "$i ";
+echo memory_get_usage()."<br />";
+  ?>
+</body>
+</html>
