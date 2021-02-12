2f8668d2a2541511032495f93c566b00b537fb9f
andrewLivancov <gilamrot@gmail.com>
2021-02-12 03:08:18 +0300

    book 256

---
+++ book-256.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$str = "Hello world";
+echo "{$str[2]}<br />"
+$str = "Привет мир!";
+echo "{$str[2]}<br />";
+  ?>
+</body>
+</html>
