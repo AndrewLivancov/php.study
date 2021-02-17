5a29f0b919a632a71df2288090facd3602d529eb
andrewLivancov <gilamrot@gmail.com>
2021-02-17 23:36:12 +0300

    book 285

---
+++ book-285.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+$A = [
+"a" => "Zero",
+"b" => "Weapon",
+"c" => "Alpha",
+"d" => "Processor"
+];
+sort($A);
+print_r($A);
+ ?>
+</body>
+</html>
