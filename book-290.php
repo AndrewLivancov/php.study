2d630ecf7f06a67c4907f38ffff9e2737c171c03
andrewLivancov <gilamrot@gmail.com>
2021-02-18 01:07:14 +0300

    book 290

---
+++ book-290.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$L1 = [10, 20, 30];
+$L2 = [100, 200, 300];
+$L = array_merge($L1, $L2);
+print_r($L);
+  ?>
+</body>
+</html>
