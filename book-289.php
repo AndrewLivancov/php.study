e0e7704fa678033bf6dba63bab59788bc6b798af
andrewLivancov <gilamrot@gmail.com>
2021-02-18 00:51:36 +0300

    book 289

---
+++ book-289.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$names = [
+"Joel" => "Silver",
+"Grant" => "Hill",
+"Andrew" => "Mason",
+];
+$names = array_flip($names);
+print_r($names);
+  ?>
+</body>
+</html>
