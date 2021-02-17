a74a98d1d6b0e37aaa1458701b1cfb1eebc36d72
andrewLivancov <gilamrot@gmail.com>
2021-02-18 00:45:25 +0300

    book 288

---
+++ book-288.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$concept = ["Banana", "Coffee", "Ice cream", "Throat" ];
+shuffle($concept);
+print_r($concept);
+?>
+</body>
+</html>
