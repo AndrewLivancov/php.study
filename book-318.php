9c0bc2a196011dd312deafaca289a275d6f25c9a
andrewLivancov <gilamrot@gmail.com>
2021-02-20 20:30:12 +0300

    book 318

---
+++ book-318.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+$path = "c:\windows\system32\drivers\etc\hosts";
+echo $path."<br />";
+$path = "c:\\non\existent\file";
+echo $path."<br />";
+  ?>
+</body>
+</html>
