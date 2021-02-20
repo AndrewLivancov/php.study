5e3d73f6de3e98149f697cfed751d2b53cdfebd0
andrewLivancov <gilamrot@gmail.com>
2021-02-20 20:51:07 +0300

    book 321

---
+++ book-321.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php 
+$f =  fopen("file.csv", "rt") or die("Ошибка!");
+for ($i = 0; $data = fgetcsv($f, 1000, ";"); $i++) {
+	$num = count($data);
+	echo "<h3>Строка номер $i (полей: $num):</h3>";
+	for ($c = 0; $c < $num; $c++)
+		print "[$c]: $data[$c]<br />";
+}
+fclose($f);
+ ?>
+</body>
+</html>
