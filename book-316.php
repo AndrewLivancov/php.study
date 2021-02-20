a2d5c2691fa97fd71defa84297d9cf12edd42608
andrewLivancov <gilamrot@gmail.com>
2021-02-20 20:19:42 +0300

    book 316

---
+++ book-316.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<?php
+function makeHex($st)
+{
+	for ($i = 0; $i < strlen($st); $i++)
+		$hex[] = sprintf("%02X", ord($st[$i]));
+	return join(" ", $hex);
+}
+$f = fopen(__FILE__, "rb");
+echo makeHex(fgets($f, 100)), "<br />\n";
+$f = fopen(__FILE__, "rt");
+echo makeHex(fgets($f, 100)), "<br />\n";
+  ?>
+</body>
+</html>
